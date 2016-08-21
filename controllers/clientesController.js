aa87487a834dfa2cd062e6d9c2767476878cde59
christian f garcia <christian.g.gml@gmail.com>
2016-08-21 12:53:45 -0600

    clientes ya funcionando

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -1,11 +1,46 @@
 "use strict";
 var clientesController = angular.module("clientesController",[])
 .controller("clientesControl",function($scope,clientesServicio){
-  clientesServicio.consultarClientes(function(res){
-    $scope.clientes = res;
-    
-  });
+
+  $scope.traerClientes = function(){
+    clientesServicio.consultarClientes(function(res){
+    $scope.clientes = res.data.data.response;
+    });
+  }
+
+  $scope.llenarClienteNuevo = function() {
+    $scope.folio = $scope.clientes.length + 1;
+    $scope.infoCliente = {};
+    $scope.form=true;
+    $scope.editar =false;
+  }
   $scope.guardarCliente = function(){
-    clientesServicio.guardarCliente(this.infoCliente);
+    clientesServicio.guardarCliente(this.infoCliente,function(){
+      $scope.traerClientes();
+      $scope.form =false;
+    });
+  }
+
+  $scope.llenarCliente = function (idCliente) {
+    var cliente = [];
+    var temp = [];
+    clientesServicio.consultarClientePorID(idCliente.id_cliente,function(res){
+      cliente = res[0];
+      $scope.editar = true;
+      $scope.folio = idCliente.id_cliente;
+      $scope.infoCliente.nomCliente = cliente.nom_cliente;
+      $scope.infoCliente.apellidoP = cliente.apellido_paterno;
+      $scope.infoCliente.apellidoM = cliente.apellido_materno;
+      $scope.infoCliente.rfc = cliente.rfc;
+      $scope.form =true;
+    });
+
+    $scope.editarCliente = function(){
+      this.infoCliente.idCliente = $scope.folio;
+      clientesServicio.editarCliente(this.infoCliente,function(){
+        $scope.traerClientes();
+        $scope.form =false;
+      });
+    }
   }
 });
--- services/clientesService.js
+++ services/clientesService.js
@@ -4,15 +4,29 @@ var clientesService = angular.module("clientesService",[])
   var url = "backend/api/clientes/";
   var clientes  = [];
   var clientesServicio = {};
+  // clientesServicio.consultarClientes = function (callback){
+  //   $http.get(url).then(function(res){
+  //     callback(res.data.data.response);
+  //   },function(err){
+  //     $.notify("Error de conexion","error");
+  //   })
+  // }
+
   clientesServicio.consultarClientes = function (callback){
-    $http.get(url).then(function(res){
+    $http.get(url).then(callback,function(err){
+      $.notify("Error de conexion","error");
+    })
+  }
+
+  clientesServicio.consultarClientePorID = function (id,callback){
+    $http.get(url + '/' + id).then(function(res){
       callback(res.data.data.response);
     },function(err){
       $.notify("Error de conexion","error");
     })
   }
 
-  clientesServicio.guardarCliente = function(cliente) {
+  clientesServicio.guardarCliente = function(cliente,callback) {
     if(!cliente.nomCliente){
       $.notify("No es posible continuar, debe ingresar nombre es obligatorio","warn");
       return;
@@ -28,9 +42,35 @@ var clientesService = angular.module("clientesService",[])
 
     $http.post(url,cliente).then(function(res){
       $.notify("Bien Hecho. El cliente ha sido registrado correctamente","success");
+      callback();
+    },function(err){
+      $.notify("Error de conexion","error")
+    });
+
+  }
+
+  clientesServicio.editarCliente = function(cliente,callback) {
+
+    if(!cliente.nomCliente){
+      $.notify("No es posible continuar, debe ingresar nombre es obligatorio","warn");
+      return;
+    }
+    if(!cliente.apellidoP){
+      $.notify("No es posible continuar, debe ingresar apellido paterno es obligatorio","warn");
+      return;
+    }
+    if(!cliente.rfc){
+      $.notify("No es posible continuar, debe ingresar RFC es obligatorio","warn");
+      return;
+    }
+
+    $http.put(url + '/' + cliente.idCliente,cliente).then(function(res){
+      $.notify("Bien Hecho. El cliente ha sido registrado correctamente","success");
+      callback();
     },function(err){
       $.notify("Error de conexion","error")
     })
   }
+
   return clientesServicio;
 })
