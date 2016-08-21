cfc589467b6923e4754686acad6a87292b510188
christian f garcia <christian.g.gml@gmail.com>
2016-08-20 23:07:50 -0600

    c

--- controllers/clientesController.js
+++ controllers/clientesController.js
@@ -3,6 +3,7 @@ var clientesController = angular.module("clientesController",[])
 .controller("clientesControl",function($scope,clientesServicio){
   clientesServicio.consultarClientes(function(res){
     $scope.clientes = res;
+    
   });
   $scope.guardarCliente = function(){
     clientesServicio.guardarCliente(this.infoCliente);
